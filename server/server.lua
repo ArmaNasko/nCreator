2d07056949f7f1173635cf9c13a886b86865b16b
Nasko <103703875+ArmaNasko@users.noreply.github.com>
2022-06-15 23:24:52 +0200

    Add files via upload

---
+++ server/server.lua
@@ -0,0 +1,37 @@
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterNetEvent("nCreator:save")
+AddEventHandler("nCreator:save", function(playerInfo)
+    local src = source
+    local player = ESX.GetPlayerFromId(src)
+
+    MySQL.Sync.execute('UPDATE users SET firstname = @firstname, lastname = @lastname, dateofbirth = @dateofbirth, sex = @sex, height = @height WHERE identifier = @identifier', {
+		['@identifier'] = player.identifier,
+		['@firstname'] = playerInfo.firstName,
+		['@lastname'] = playerInfo.lastName,
+		['@dateofbirth'] = playerInfo.dateOfBirth,
+		['@sex'] = playerInfo.sex,
+		['@height'] = playerInfo.height
+	})
+end)
+
+RegisterServerEvent("nCreator:setPlayerToBucket")
+AddEventHandler("nCreator:setPlayerToBucket", function(id)
+    SetPlayerRoutingBucket(source, id)
+end)
+
+RegisterServerEvent("nCreator:setPlayerToNormalBucket")
+AddEventHandler("nCreator:setPlayerToNormalBucket", function()
+    SetPlayerRoutingBucket(source, 0)
+end)
+
+RegisterServerEvent("esx_skin:save")
+AddEventHandler("esx_skin:save", function(skin)
+    local src = source 
+    local xPlayer = ESX.GetPlayerFromId(src)
+
+    MySQL.Async.execute('UPDATE users SET skin = @skin WHERE identifier = @identifier', {
+        ['@skin'] = json.encode(skin), 
+        ['@identifier'] = xPlayer.identifier
+    })
+end)
---
+++ shared/shared.lua
@@ -0,0 +1,38 @@
+Creator = {
+    spawnPos = {x = -1037.789, y = -2737.651, z = 20.16928},
+    spawnNotif = "Bienvenue sur le ~b~server~s~ !",
+    outfit = {
+        Chic = {
+            male = {
+                ['tshirt_1'] = 31, ['tshirt_2'] = 0,
+                ['torso_1'] = 32, ['torso_2'] = 0,
+                ['arms'] = 27,
+                ['pants_1'] = 24, ['pants_2'] = 0,
+                ['shoes_1'] = 10, ['shoes_2'] = 0,
+            },
+            female = {
+                ['tshirt_1'] = 38,['tshirt_2'] = 0,
+                ['torso_1'] = 57, ['torso_2'] = 0,
+                ['arms'] = 21, ['arms_2'] = 0,
+                ['pants_1'] = 11, ['pants_2'] = 1,
+                ['shoes_1'] = 49, ['shoes_2'] = 0,
+            },
+        },
+        Decontract = {
+            male = {
+                ['tshirt_1'] = 15, ['tshirt_2'] = 0,
+                ['torso_1'] = 86, ['torso_2'] = 0,
+                ['arms'] = 1,
+                ['pants_1'] = 5, ['pants_2'] = 2,
+                ['shoes_1'] = 8, ['shoes_2'] = 2,
+            },
+            female = {
+                ['tshirt_1'] = 60,['tshirt_2'] = 0,
+                ['torso_1'] = 65, ['torso_2'] = 7,
+                ['arms'] = 6, ['arms_2'] = 0,
+                ['pants_1'] = 89, ['pants_2'] = 22,
+                ['shoes_1'] = 8, ['shoes_2'] = 11,
+            },
+        },
+    },
+}
